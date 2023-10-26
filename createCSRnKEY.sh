50df5dc8e62552cfd74d9a13bee288aa448130f6
Ali-anc <68405551+Ali-anc@users.noreply.github.com>
2023-10-26 13:38:56 -0400

    Update createCSRnKEY.sh
    Added timestamp with separate folders to prevent overwriting

--- createCSRnKEY.sh
+++ createCSRnKEY.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # The following uses fixed values for your bussiness or organization, and outputs the .csr based on that. These values can be changed within the script.
+# I've included a timestamp that, when combined with the inputted domain, ensures that keys and CSRs are created in separate folders to prevent overwriting.
 
 # Check if domain name is provided as an argument
 if [ $# -ne 1 ]; then
@@ -9,12 +10,17 @@ if [ $# -ne 1 ]; then
 fi
 
 domain_name=$1
+timestamp=$(date +"%Y%m%d%H%M%S") # Get the current timestamp
 
 # Check if domain name ends with a TLD or eTLD
 if ! [[ $domain_name =~ ^[a-zA-Z0-9]+(\.[a-zA-Z]{2,}){1}$ ]]; then
   echo "Invalid domain name. Please provide a domain name with a valid TLD or eTLD and exactly one dot (e.g. example.com, example.io, example.org)."
   exit 1
 fi
+
+# Create a folder with the domain name and timestamp
+folder_name="${domain_name}_${timestamp}"
+mkdir -p "/home/somepath/cert/$folder_name"
 
 # Set fixed values
 country=US
@@ -26,12 +32,15 @@ email="ancdata@example.com"
 common_name="$domain_name"
 
 # Create CSR and KEY
-openssl req -new -newkey rsa:2048 -nodes -keyout $domain_name.key -out $domain_name.csr -subj "/C=$country/ST=$state/L=$locality/O=$org_name/OU=$org_unit_name/CN=$common_name/emailAddress=$email"
+openssl req -new -newkey rsa:2048 -nodes -keyout "/home/somepath/cert/$folder_name/$domain_name.key" -out "/home/somepath/cert/$folder_name/$domain_name.csr" -subj "/C=$country/ST=$state/L=$locality/O=$org_name/OU=$org_unit_name/CN=$common_name/emailAddress=$email"
 
-echo "Certificate Signing Request (.csr) and Private Key (.key) files created for $domain_name"
+echo "Certificate Signing Request (.csr) and Private Key (.key) files created for /home/somepath/cert/$folder_name/$domain_name"
 
+
+: '
 # Display CSR and private key
 echo "Certificate Signing Request:"
 cat $domain_name.csr
 echo "Private Key:"
 cat $domain_name.key
+'
